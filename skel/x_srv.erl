8c483d67c2236581d72d2c333f78ace0900c3796
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2013-10-27 18:34:23 +0100

    Generic gen_server framework.

---
+++ skel/x_srv.erl
@@ -0,0 +1,45 @@
+-module(x_srv).
+
+-behaviour(gen_server).
+
+-export([
+	code_change/3, handle_call/3, handle_cast/2, handle_info/2, init/1, terminate/2
+	]).
+	
+-export([
+	start_link/0
+	]).
+	
+-record(state, {
+	}).
+
+%% API
+
+start_link() ->
+	gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+
+%% Callbacks
+
+init([]) ->
+	{ok, #state{}}.
+	
+handle_call(Msg, _From, State) ->
+	lager:error("Unknown call: ~p", [Msg]),
+	{reply, {error, unknown}, State}.
+	
+handle_cast(Msg, State) ->
+	lager:error("Unknown cast: ~p", [Msg]),
+	{noreply, State}.
+	
+handle_info(Msg, State) ->
+	lager:error("Unknown handle_info: ~p", [Msg]),
+	{noreply, State}.
+	
+terminate(_Reason, _State) ->
+	ok.
+	
+code_change(_OldVsn, State, _Extra) ->
+	{ok, State}.
+
+
+	

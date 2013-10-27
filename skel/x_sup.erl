b060f81a31d44be8a501dfa430e0aca45f79eeb1
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2013-10-27 16:23:54 +0100

    Introduce some skeleton files.

---
+++ skel/x_sup.erl
@@ -0,0 +1,27 @@
+-module(x_sup).
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+%% Helper macro for declaring children of supervisor
+-define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
+
+%% ===================================================================
+%% API functions
+%% ===================================================================
+
+start_link() ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+%% ===================================================================
+%% Supervisor callbacks
+%% ===================================================================
+
+init([]) ->
+	Strategy = {one_for_all, 10, 3600},
+	{ok, {Strategy, []}}.
